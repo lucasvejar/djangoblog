345a6166da80c914559d25ea773b614c1ea2ab11
Lucas Vejar <lucasficus@gmail.com>
2020-06-08 16:14:58 -0300

    linkeando primera vista

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hello dear web page")
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {}) 
