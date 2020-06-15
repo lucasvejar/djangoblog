13d158c5ff69ac00203e01e2371641961373f1f5
Lucas Vejar <lucasficus@gmail.com>
2020-06-15 16:39:02 -0300

    problem with static folder

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Update database configuration with $DATABASE_URL.
 import dj_database_url  
